d69edd62e8a7f771f07c4f08cf1d2db9c43da28a
SajaAsfour <sajanazih2004@gmail.com>
2026-01-09 00:36:11 +0200

    task

---
+++ MvcTask1/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcTask1.Data;
+using MvcTask1.Models;
+
+namespace MvcTask1.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public ViewResult Index()
+        {
+            var categories = context.Categories;
+            return View("Index", categories);
+        }
+
+        public ViewResult Details(int id) 
+        {
+            var category = context.Categories.Find(id);
+            return View("Details" ,category);
+        }
+
+        public ViewResult Create()
+        {
+            return View("Create");
+        }
+        public ViewResult Store(Category request)
+        {
+            context.Categories.Add(request);
+            context.SaveChanges();
+            var categories = context.Categories;
+            return View("Index" ,categories);
+        }
+
+        public ViewResult Delete(int id) 
+        {
+            var category = context.Categories.Find(id);
+            context.Categories.Remove(category);
+            context.SaveChanges();
+            var categories = context.Categories;
+            return View("Index" ,categories);
+        }
+    }
+}
---
+++ MvcTask1/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcTask1.Models;
+
+namespace MvcTask1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;DataBase=MVCTask1;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
+        }
+    }
+}
